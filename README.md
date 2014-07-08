1cc3f8219ef31476fce79ea757eaef5acf653cbb
Jeremy Fields <jeremy.fields@viget.com>
2014-07-08 06:34:27 -0600

    Adding github pages

--- README.md
+++
@@ -1,6 +0,0 @@
-infieldLabel
-============
-
-A jQuery plugin to make infield form labels suck less.
-
-Read the full post: http://viget.com/inspire/making-infield-form-labels-suck-less-2
--- jquery.infieldLabel.js
+++ jquery.infieldLabel.js
@@ -1,4 +1,4 @@
-/*! jquery.infieldLabel.js v 1.0 | Author: Jeremy Fields [jeremy.fields@viget.com], 2013 | License: MIT */
+/*! jquery.infieldLabel.js v 1.0.1 | Author: Jeremy Fields [jeremy.fields@viget.com], 2014 | License: MIT */
 
 (function($){
 
@@ -10,10 +10,6 @@
 
 		// Access to jQuery and DOM versions of element
 		base.$el = $(el);
-		base.el = el;
-
-		// Add a reverse reference to the DOM object
-		base.$el.data("infieldLabel", base);
 
 		// internal variables
 		base.$input = null;
@@ -30,8 +26,8 @@
 
 		// first time input setup
 		base.setup = function() {
-			base.$input = base.$el.find("input[type=text],input[type=password]");
-			base.$label = base.$el.find("label");
+			base.$input = base.$el.find('input');
+			base.$label = base.$el.find('label');
 
 			// hide label if there's already a value
 			base.blur();
@@ -43,28 +39,29 @@
 		// binds the focus, blur and change events
 		base.bind = function() {
 			base.$input
-				.on("focus.infield", function() {
+				.on('focus.infield', function() {
 					base.$el
 						.removeClass(base.options.hideClass)
 						.addClass(base.options.focusClass);
 
-				}).on("blur.infield change.infield", function() {
+				}).on('blur.infield change.infield', function() {
 					base.blur();
 				});
-				base.$label.on("click.infield", function() {
+
+				base.$label.on('click.infield', function() {
 					// empty onclick to fix older ios devices
 					// http://stackoverflow.com/a/6472181/222155
 				});
 		};
 
 		base.blur = function() {
-			if (base.$input.val() !== "") {
+			if (base.$input.val() !== '') {
 				base.$el
 					.removeClass(base.options.focusClass)
 					.addClass(base.options.hideClass);
 
 			} else {
-				base.$el.removeClass(base.options.focusClass + " " + base.options.hideClass);
+				base.$el.removeClass(base.options.focusClass + ' ' + base.options.hideClass);
 			}
 		};
 
@@ -74,11 +71,11 @@
 	};
 
 	$.infieldLabel.defaultOptions = {
-		focusClass: "placeholder-focus",
-		hideClass: "placeholder-hide"
+		focusClass: 'placeholder-focus',
+		hideClass: 'placeholder-hide'
 	};
 
-	$.fn.infieldLabel = function(options){
+	$.fn.infieldLabel = function(options) {
 		this.each(function(){
 			(new $.infieldLabel(this, options));
 		});
