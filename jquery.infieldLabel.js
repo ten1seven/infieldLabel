bb1dde5ba54634a9c500d309098ac768e8711c30
Jeremy Fields <jeremy.fields@viget.com>
2016-04-11 16:57:09 -0600

    Fix white space and bump version

--- jquery.infieldLabel.js
+++ jquery.infieldLabel.js
@@ -1,86 +1,98 @@
-/*! jquery.infieldLabel.js v 1.0.1 | Author: Jeremy Fields [jeremy.fields@viget.com], 2014 | License: MIT */
+(function($) {
 
-(function($){
+  $.infieldLabel = function(el, options) {
 
-	$.infieldLabel = function(el, options){
+    // To avoid scope issues, use 'base' instead of 'this'
+    // to reference this class from internal events and functions.
+    var base = this;
 
-		// To avoid scope issues, use 'base' instead of 'this'
-		// to reference this class from internal events and functions.
-		var base = this;
+    // Access to jQuery and DOM versions of element
+    base.$el = $(el);
 
-		// Access to jQuery and DOM versions of element
-		base.$el = $(el);
+    // internal variables
+    base.$input = null;
 
-		// internal variables
-		base.$input = null;
-
-		base.init = function() {
-			base.options = $.extend({}, $.infieldLabel.defaultOptions, options);
-
-			base.setup();
-		};
+    base.init = function() {
+      base.options = $.extend({}, $.infieldLabel.defaultOptions, options);
+      base.setup();
+    };
 
 
-		// setup
-		// ==========================================================================
+    /*
+      --------------------
+      Set up
+      --------------------
+    */
 
-		// first time input setup
-		base.setup = function() {
-			base.$input = base.$el.find('input');
-			base.$label = base.$el.find('label');
+    // first time input setup
+    base.setup = function() {
+      base.$input = base.$el.find('input');
+      base.$label = base.$el.find('label');
 
-			// hide label if there's already a value
-			base.blur();
+      // hide label if there's already a value
+      base.blur();
 
-			// bind events
-			base.bind();
-		};
+      // bind events
+      base.bind();
+    };
 
-		// binds the focus, blur and change events
-		base.bind = function() {
-			base.$input
-				.on('focus.infield', function() {
-					base.$el
-						.removeClass(base.options.hideClass)
-						.addClass(base.options.focusClass);
+    // binds the focus, blur and change events
+    base.bind = function() {
+      base.$input
+        .on('focus.infield', function() {
+          base.$el
+            .removeClass(base.options.hideClass)
+            .addClass(base.options.focusClass);
 
-				}).on('blur.infield change.infield', function() {
-					base.blur();
-				});
+        }).on('blur.infield change.infield', function() {
+          base.blur();
+        });
 
-				base.$label.on('click.infield', function() {
-					base.$el
-						.removeClass(base.options.hideClass)
-						.addClass(base.options.focusClass);
-					base.$input.focus()
-				});
-		};
+        base.$label.on('click.infield', function() {
+          base.$el
+            .removeClass(base.options.hideClass)
+            .addClass(base.options.focusClass);
 
-		base.blur = function() {
-			if (base.$input.val() !== '') {
-				base.$el
-					.removeClass(base.options.focusClass)
-					.addClass(base.options.hideClass);
+          base.$input.focus();
+        });
+    };
 
-			} else {
-				base.$el.removeClass(base.options.focusClass + ' ' + base.options.hideClass);
-			}
-		};
+    base.blur = function() {
+      if (base.$input.val() !== '') {
+        base.$el
+          .removeClass(base.options.focusClass)
+          .addClass(base.options.hideClass);
 
-		// run initializer
-		// ==========================================================================
-		base.init();
-	};
+      } else {
+        base.$el
+          .removeClass(base.options.focusClass + ' ' + base.options.hideClass);
+      }
+    };
 
-	$.infieldLabel.defaultOptions = {
-		focusClass: 'placeholder-focus',
-		hideClass: 'placeholder-hide'
-	};
+    /*
+      --------------------
+      Initialize
+      --------------------
+    */
+    base.init();
+  };
 
-	$.fn.infieldLabel = function(options) {
-		this.each(function(){
-			(new $.infieldLabel(this, options));
-		});
-	};
+
+  /*
+    --------------------
+    Options
+    --------------------
+  */
+
+  $.infieldLabel.defaultOptions = {
+    focusClass: 'placeholder-focus',
+    hideClass: 'placeholder-hide'
+  };
+
+  $.fn.infieldLabel = function(options) {
+    this.each(function() {
+      new $.infieldLabel(this, options);
+    });
+  };
 
 })(jQuery);
